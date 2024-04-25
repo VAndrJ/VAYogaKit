6d8a70ba6f4fca4b37e83c4a8a0348d9526d3d88
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-25 21:44:07 +0300

    [core] Yoga supporting extensions.

--- Sources/VAYogaKit/YG+Support.swift
+++ Sources/VAYogaKit/YG+Support.swift
@@ -8,6 +8,18 @@
 import Foundation
 import yoga
 
+extension YGEdge {
+    static let all: YGEdge = YGEdgeAll
+    static let left: YGEdge = YGEdgeLeft
+    static let top: YGEdge = YGEdgeTop
+    static let right: YGEdge = YGEdgeRight
+    static let bottom: YGEdge = YGEdgeBottom
+    static let start: YGEdge = YGEdgeStart
+    static let end: YGEdge = YGEdgeEnd
+    static let horizontal: YGEdge = YGEdgeHorizontal
+    static let vertical: YGEdge = YGEdgeVertical
+}
+
 extension YGDirection {
     static let ltr: YGDirection = YGDirectionLTR
     static let rtl: YGDirection = YGDirectionRTL
