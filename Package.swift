dbc3032dac7c094029a575d2d8bb28dcc7f57f33
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-25 20:19:23 +0300

    [core] Package.swift

--- Package.swift
+++ Package.swift
@@ -1,23 +1,29 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "VAYogaKit",
+    platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "VAYogaKit",
-            targets: ["VAYogaKit"]),
+            targets: ["VAYogaKit"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/facebook/yoga.git", exact: "3.0.4"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "VAYogaKit"),
+            name: "VAYogaKit",
+            dependencies: [
+                .product(name: "yoga", package: "yoga"),
+            ]
+        ),
         .testTarget(
             name: "VAYogaKitTests",
-            dependencies: ["VAYogaKit"]),
+            dependencies: ["VAYogaKit"]
+        ),
     ]
 )
