3ed3a2aca9196b9ccefc64c30f62f4a41aa3ae8d
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-25 21:41:48 +0300

    [tests] Added.

--- Tests/VAYogaKitTests/VAYogaKitTests.swift
+++ Tests/VAYogaKitTests/VAYogaKitTests.swift
@@ -1,12 +1,12 @@
 import XCTest
 @testable import VAYogaKit
+import yoga
 
 final class VAYogaKitTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    func test_constants_direction() {
+        XCTAssertEqual(YGDirectionLTR, .ltr)
+        XCTAssertEqual(YGDirectionRTL, .rtl)
+        XCTAssertEqual(YGDirectionInherit, .inherit)
     }
 }
