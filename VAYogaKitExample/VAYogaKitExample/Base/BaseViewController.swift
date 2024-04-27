e4b43899f1b946c5a64e9109628e2c7b3e5c06f8
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-27 12:44:08 +0300

    [example] Base controller.

---
+++ VAYogaKitExample/VAYogaKitExample/Base/BaseViewController.swift
@@ -0,0 +1,9 @@
+//
+//  BaseViewController.swift
+//  VAYogaKitExample
+//
+//  Created by VAndrJ on 27.04.2024.
+//  Copyright © 2024 Volodymyr Andriienko. All rights reserved.
+//
+
+import Foundation
