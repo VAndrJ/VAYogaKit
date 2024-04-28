dba50d22ea3e734e5c991eda413d63de5acf64e4
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-28 10:57:25 +0300

    [tests] Yoga node child.

--- Tests/VAYogaKitTests/YGNodeTests.swift
+++ Tests/VAYogaKitTests/YGNodeTests.swift
@@ -12,6 +12,62 @@ import yoga
 final class YGNodeTests: XCTestCase {
 
     @MainActor
+    func test_node_childAddingAndRemoving() {
+        let position = 0
+        let (parentNode, childNode) = generateSUT()
+        checkNodeInserting(parentNode: parentNode, childNode: childNode, position: position)
+
+        parentNode.removeAllChildren()
+        checkChildrenAreEmpty(parentNode: parentNode, childNode: childNode, position: position)
+
+        checkNodeInserting(parentNode: parentNode, childNode: childNode, position: position)
+        childNode.removeFromParent()
+        checkChildrenAreEmpty(parentNode: parentNode, childNode: childNode, position: position)
+
+        checkNodeInserting(parentNode: parentNode, childNode: childNode, position: position)
+        parentNode.remove(child: childNode)
+        checkChildrenAreEmpty(parentNode: parentNode, childNode: childNode, position: position)
+    }
+
+    @MainActor
+    private func checkChildrenAreEmpty(
+        parentNode: YGNodeRef,
+        childNode: YGNodeRef,
+        position: Int,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        XCTAssertNil(parentNode.getGhild(at: position))
+        XCTAssertEqual(0, parentNode.childCount)
+    }
+
+    @MainActor
+    private func checkNodeInserting(
+        parentNode: YGNodeRef,
+        childNode: YGNodeRef,
+        position: Int,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        parentNode.insert(child: childNode, at: position)
+
+        XCTAssertEqual(childNode, parentNode.getGhild(at: position), file: file, line: line)
+        XCTAssertEqual(parentNode, childNode.parent, file: file, line: line)
+        XCTAssertEqual(1, parentNode.childCount, file: file, line: line)
+    }
+
+    @MainActor
+    private func generateSUT(file: StaticString = #filePath, line: UInt = #line) -> (parent: YGNodeRef, child: YGNodeRef) {
+        let parentNode = createNode()
+
+        XCTAssertEqual(0, parentNode.childCount, file: file, line: line)
+
+        let childNode = createNode()
+
+        return (parentNode, childNode)
+    }
+
+    @MainActor
     private func createNode() -> YGNodeRef {
         YGNodeRef.new(for: NSObject())
     }
