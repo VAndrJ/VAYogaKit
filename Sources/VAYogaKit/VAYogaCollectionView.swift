5d5501c788d64df98eca9ad7d74d46f6bd65cd05
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-28 15:27:05 +0300

    [core] Yoga base collection view implementation.

--- Sources/VAYogaKit/VAYogaCollectionView.swift
+++ Sources/VAYogaKit/VAYogaCollectionView.swift
@@ -1,8 +1,30 @@
 //
 //  VAYogaCollectionView.swift
-//  
+//
 //
 //  Created by VAndrJ on 28.04.2024.
 //
 
-import Foundation
+import UIKit
+import yoga
+
+open class VAYogaCollectionView: UICollectionView, VAYogaLayout {
+    public let layoutType: VAYogaLayoutType = .view
+    public var node: YGNodeRef!
+    public var sublayouts: [any VAYogaLayout] = []
+    open var layout: any VAYogaLayout { self }
+
+    public init(collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(
+            frame: .init(x: 0, y: 0, width: 240, height: 128),
+            collectionViewLayout: layout
+        )
+
+        self.node = .new(for: self)
+    }
+
+    @available(*, unavailable)
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
