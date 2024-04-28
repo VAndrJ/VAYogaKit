0ab122a8e49b842e0f1251b11c97f6b257a6bd05
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-28 16:28:48 +0300

    [core] Yoga row layout component.

--- Sources/VAYogaKit/VAYogaLayout+Components.swift
+++ Sources/VAYogaKit/VAYogaLayout+Components.swift
@@ -52,3 +52,48 @@ open class Column: VAYogaLayout {
         YGNodeFree(node)
     }
 }
+
+open class Row: VAYogaLayout {
+    public let layoutType: VAYogaLayoutType = .layout
+    public var node: YGNodeRef!
+    public var frame: CGRect = .zero
+    public var sublayouts: [any VAYogaLayout]
+    public var layout: any VAYogaLayout { self }
+
+    @MainActor
+    public init(
+        spacing: Float = 0,
+        main: YGJustify = .start,
+        cross: YGAlign = .start,
+        wrap: YGWrap = .noWrap,
+        row: Float = 0,
+        alignContent: YGAlign = .start,
+        isReversed: Bool = false,
+        @VAYogaLayoutBuilder content: () -> [VAYogaLayout]
+    ) {
+        self.sublayouts = content()
+
+        node = .new(for: self)
+        if isReversed {
+            node.flexDirection = .rowReverse
+        } else {
+            node.flexDirection = .row
+        }
+        node.columnGap = spacing
+        node.rowGap = row
+        node.justifyContent = main
+        node.alignItems = cross
+        node.flexWrap = wrap
+        node.alignContent = alignContent
+    }
+
+    public func sizeThatFits(_ size: CGSize) -> CGSize {
+        .zero
+    }
+
+    public func setNeedsLayout() {}
+
+    deinit {
+        YGNodeFree(node)
+    }
+}
