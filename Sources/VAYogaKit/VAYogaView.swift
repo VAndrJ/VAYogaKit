076aa67dada7d61468fe8f9b499ede86b9b568d0
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-28 15:14:56 +0300

    [core] Yoga view implementation.

--- Sources/VAYogaKit/VAYogaView.swift
+++ Sources/VAYogaKit/VAYogaView.swift
@@ -5,4 +5,39 @@
 //  Created by VAndrJ on 27.04.2024.
 //
 
-import Foundation
+import UIKit
+import yoga
+
+open class VAYogaView: UIView, VAYogaLayout {
+    public let layoutType: VAYogaLayoutType
+    public var node: YGNodeRef!
+    public var sublayouts: [any VAYogaLayout] = []
+    open var layout: any VAYogaLayout { self }
+
+    public init(layoutType: VAYogaLayoutType) {
+        self.layoutType = layoutType
+
+        super.init(frame: .init(x: 0, y: 0, width: 240, height: 128))
+
+        self.node = .new(for: self)
+    }
+
+    @available(*, unavailable)
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+
+        flattenLayoutIfNeeded(in: self)
+        if layoutType == .root {
+            layout(mode: .fitContainer)
+        }
+    }
+
+    deinit {
+        YGNodeFree(node)
+    }
+}
+
