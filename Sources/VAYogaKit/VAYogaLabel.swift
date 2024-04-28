2679cdbcf1756a6047d5140a61a2e12fd3c34216
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-28 15:30:05 +0300

    [core] Yoga base label implementation.

--- Sources/VAYogaKit/VAYogaLabel.swift
+++ Sources/VAYogaKit/VAYogaLabel.swift
@@ -5,4 +5,31 @@
 //  Created by VAndrJ on 28.04.2024.
 //
 
-import Foundation
+import UIKit
+import yoga
+
+open class VAYogaLabel: UILabel, VAYogaLayout {
+    public let layoutType: VAYogaLayoutType = .selfSizedView
+    public var node: YGNodeRef!
+    public var sublayouts: [any VAYogaLayout] = []
+    public var layout: any VAYogaLayout { self }
+
+    open override var text: String? {
+        get { super.text }
+        set {
+            super.text = newValue
+            node.markDirtyIfAvailable()
+        }
+    }
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        self.node = .new(for: self)
+    }
+
+    @available(*, unavailable)
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
