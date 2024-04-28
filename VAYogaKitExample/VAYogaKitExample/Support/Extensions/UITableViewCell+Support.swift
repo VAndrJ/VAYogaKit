a0db1c9107078796e6e311ea2e84e368795faa7b
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-28 22:28:33 +0300

    [example] table cell supporting functions.

--- VAYogaKitExample/VAYogaKitExample/Support/Extensions/UITableViewCell+Support.swift
+++ VAYogaKitExample/VAYogaKitExample/Support/Extensions/UITableViewCell+Support.swift
@@ -9,3 +9,20 @@
 import UIKit
 
 extension UITableViewCell: VAIdentifiable {}
+
+@MainActor
+class AnyTableCellViewModel: Equatable {
+    nonisolated static func == (lhs: AnyTableCellViewModel, rhs: AnyTableCellViewModel) -> Bool {
+        type(of: lhs) == type(of: rhs) && lhs.isEqual(to: rhs)
+    }
+
+    class var cellType: (UITableViewCell & VAIdentifiable).Type { fatalError() }
+
+    var cellType: (UITableViewCell & VAIdentifiable).Type { Self.cellType }
+
+    func configure(cell: UITableViewCell) {}
+
+    nonisolated func isEqual(to other: AnyTableCellViewModel) -> Bool {
+        ObjectIdentifier(self) == ObjectIdentifier(other)
+    }
+}
