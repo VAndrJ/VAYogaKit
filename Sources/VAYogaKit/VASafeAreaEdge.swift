d9dd86fbec1488041fd6e227c0b2078a0172f319
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-28 16:23:43 +0300

    [core] Safe area edges options set.

--- Sources/VAYogaKit/VASafeAreaEdge.swift
+++ Sources/VAYogaKit/VASafeAreaEdge.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+/// A set of options representing safe area edges.
+public struct VASafeAreaEdge: RawRepresentable, OptionSet, Sendable {
+    public var rawValue: UInt
+
+    public init(rawValue: UInt) {
+        self.rawValue = rawValue
+    }
+
+    public static let top = VASafeAreaEdge(rawValue: 1 << 1)
+    public static let left = VASafeAreaEdge(rawValue: 1 << 2)
+    public static let bottom = VASafeAreaEdge(rawValue: 1 << 3)
+    public static let right = VASafeAreaEdge(rawValue: 1 << 4)
+    public static let vertical: VASafeAreaEdge = [top, bottom]
+    public static let horizontal: VASafeAreaEdge = [left, right]
+    public static let all: VASafeAreaEdge = [top, left, bottom, right]
+}
