ef786a98b48625c3cd6899232cb64315c0d42c34
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-28 15:25:01 +0300

    [core] Yoga base table view cell implementation.

--- Sources/VAYogaKit/VAYogaTableViewCell.swift
+++ Sources/VAYogaKit/VAYogaTableViewCell.swift
@@ -8,31 +8,31 @@
 import UIKit
 import yoga
 
-class VAYogaTableViewCell: UITableViewCell, VAYogaLayout {
-    var layoutType: VAYogaLayoutType = .container
-    var node: YGNodeRef!
-    var sublayouts: [any VAYogaLayout] = []
-    var layout: any VAYogaLayout { self }
+open class VAYogaTableViewCell: UITableViewCell, VAYogaLayout {
+    public var layoutType: VAYogaLayoutType = .container
+    public var node: YGNodeRef!
+    public var sublayouts: [any VAYogaLayout] = []
+    open var layout: any VAYogaLayout { self }
 
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    public override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
 
         self.node = .new(for: self)
     }
 
     @available(*, unavailable)
-    required init?(coder: NSCoder) {
+    public required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    override func layoutSubviews() {
+    public override func layoutSubviews() {
         super.layoutSubviews()
 
         flattenLayoutIfNeeded(in: contentView)
         layout(mode: .adjustHeight)
     }
 
-    override func sizeThatFits(_ size: CGSize) -> CGSize {
+    public override func sizeThatFits(_ size: CGSize) -> CGSize {
         flattenLayoutIfNeeded(in: contentView)
         layout(mode: .adjustHeight)
 
