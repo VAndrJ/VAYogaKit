db29af57565bad8851ef19963530f39803b33de1
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-28 15:16:35 +0300

    [example] Base controller implementation.

--- VAYogaKitExample/VAYogaKitExample/Base/BaseViewController.swift
+++ VAYogaKitExample/VAYogaKitExample/Base/BaseViewController.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2024 Volodymyr Andriienko. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+@MainActor
+protocol ControllerView: UIView {}
+
+class BaseViewController<V: ControllerView>: UIViewController {
+    let contentView: V
+
+    init(view: V) {
+        self.contentView = view
+
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func loadView() {
+        view = contentView
+    }
+}
