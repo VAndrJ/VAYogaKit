aa5534d6d3549ab364d54c8c5f2b6be419da4c4f
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-28 17:04:30 +0300

    [core] Yoga layout supporting extensions.

--- Sources/VAYogaKit/VAYogaLayout+UI.swift
+++ Sources/VAYogaKit/VAYogaLayout+UI.swift
@@ -6,3 +6,40 @@
 //
 
 import UIKit
+
+public extension VAYogaLayout where Self: UIView {
+
+    // TODO: - Layout caching?
+    @MainActor
+    func flattenLayoutIfNeeded(in root: UIView) {
+        let layout = self.layout
+        if layout !== self {
+            sublayouts = [layout]
+        }
+        var sublayoutViews: [UIView] = []
+
+        func appendSubviews(sublayout: VAYogaLayout) {
+            if let view = sublayout as? UIView {
+                sublayoutViews.append(view)
+            } else {
+                sublayout.sublayouts.forEach(appendSubviews(sublayout:))
+            }
+        }
+
+        sublayouts.forEach(appendSubviews(sublayout:))
+        var viewsToDelete: [UIView] = []
+        var viewToAppend: [UIView] = []
+        for view in sublayoutViews {
+            if !root.subviews.contains(view) {
+                viewToAppend.append(view)
+            }
+        }
+        for view in root.subviews {
+            if !sublayoutViews.contains(view) {
+                viewsToDelete.append(view)
+            }
+        }
+        viewsToDelete.forEach { $0.removeFromSuperview() }
+        viewToAppend.forEach { root.addSubview($0) }
+    }
+}
