da55b4b942ec8a754ee929b5c42bd88ac69df12b
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-28 15:59:33 +0300

    [example] Supporting extensions.

--- VAYogaKitExample/VAYogaKitExample/Support/Extensions/UICollectionView+Support.swift
+++ VAYogaKitExample/VAYogaKitExample/Support/Extensions/UICollectionView+Support.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2024 Volodymyr Andriienko. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UICollectionView {
+
+    func register<Cell: UICollectionViewCell & VAIdentifiable>(_ cell: Cell.Type) {
+        register(cell, forCellWithReuseIdentifier: cell.identifier)
+    }
+
+    func dequeue<Cell: UICollectionViewCell & VAIdentifiable>(
+        _ cell: Cell.Type,
+        for indexPath: IndexPath
+    ) -> Cell {
+        dequeueReusableCell(withReuseIdentifier: cell.identifier, for: indexPath) as! Cell
+    }
+}
