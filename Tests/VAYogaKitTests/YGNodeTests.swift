7cd3db15b398f194227ceb5e596cf0c834bf93e7
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-28 12:50:35 +0300

    [tests] Yoga supporting extensions.

--- Tests/VAYogaKitTests/YGNodeTests.swift
+++ Tests/VAYogaKitTests/YGNodeTests.swift
@@ -12,6 +12,18 @@ import yoga
 final class YGNodeTests: XCTestCase {
 
     @MainActor
+    func test_node_paddingRight() {
+        var node = createNode()
+        let expected: YGValue = .point(value: 10)
+
+        XCTAssertNotEqual(expected, node.paddingRight)
+
+        node.paddingRight = expected
+
+        XCTAssertEqual(expected, node.paddingRight)
+    }
+
+    @MainActor
     func test_node_paddingBottom() {
         var node = createNode()
         let expected: YGValue = .point(value: 10)
