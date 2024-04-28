d0184552e87a6c4af5ef0ba0ea8784b59872107e
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-28 16:07:50 +0300

    [example] Main table view cell

---
+++ VAYogaKitExample/VAYogaKitExample/DesignComponents/Cells/MainTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  MainTableViewCell.swift
+//  VAYogaKitExample
+//
+//  Created by VAndrJ on 28.04.2024.
+//  Copyright © 2024 Volodymyr Andriienko. All rights reserved.
+//
+
+import Foundation
