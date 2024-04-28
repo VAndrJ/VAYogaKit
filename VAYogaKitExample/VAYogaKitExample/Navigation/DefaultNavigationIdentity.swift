9f136b9e866dad06884e0ffb6c8c011a2c9eb46f
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-28 17:23:16 +0300

    [example] Navigation added.

---
+++ VAYogaKitExample/VAYogaKitExample/Navigation/DefaultNavigationIdentity.swift
@@ -0,0 +1,9 @@
+//
+//  DefaultNavigationIdentity.swift
+//  VAYogaKitExample
+//
+//  Created by VAndrJ on 28.04.2024.
+//  Copyright © 2024 Volodymyr Andriienko. All rights reserved.
+//
+
+import Foundation
