32eab77c38a91fd2c6866e31da3fe526e2d18685
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-28 13:50:28 +0300

    [core] Yoga layout type.

--- Sources/VAYogaKit/VAYogaLayout.swift
+++ Sources/VAYogaKit/VAYogaLayout.swift
@@ -5,9 +5,18 @@
 //  Created by VAndrJ on 26.04.2024.
 //
 
-import Foundation
+import UIKit
 import yoga
 
 public protocol VAYogaLayout: AnyObject {
+    @MainActor var layoutType: VAYogaLayoutType { get }
     @MainActor var node: YGNodeRef! { get set }
+}
+
+public enum VAYogaLayoutType {
+    case layout
+    case view
+    case root
+    case container
+    case selfSizedView
 }
