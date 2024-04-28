b83c2ac066e42b1c7f3aefe5483bdfee7fabd6f6
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-28 15:21:29 +0300

    [core] Yoga base table view.

--- Sources/VAYogaKit/VAYogaTableView.swift
+++ Sources/VAYogaKit/VAYogaTableView.swift
@@ -5,4 +5,25 @@
 //  Created by VAndrJ on 28.04.2024.
 //
 
-import Foundation
+import UIKit
+import yoga
+
+open class VAYogaTableView: UITableView, VAYogaLayout {
+    public var layoutType: VAYogaLayoutType = .view
+    public var node: YGNodeRef!
+    public var sublayouts: [any VAYogaLayout] = []
+    open var layout: any VAYogaLayout { self }
+
+    public init(style: UITableView.Style, estimatedRowHeight: CGFloat = 44) {
+        super.init(frame: .init(x: 0, y: 0, width: 240, height: 128), style: style)
+
+        self.node = .new(for: self)
+        self.rowHeight = UITableView.automaticDimension
+        self.estimatedRowHeight = estimatedRowHeight
+    }
+
+    @available(*, unavailable)
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
