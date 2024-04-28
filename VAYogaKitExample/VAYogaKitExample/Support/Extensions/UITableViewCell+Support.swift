79eddde36beaca4f52e39ffbc41319832e83373e
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-28 15:55:30 +0300

    [example] Supporting extensions.

---
+++ VAYogaKitExample/VAYogaKitExample/Support/Extensions/UITableViewCell+Support.swift
@@ -0,0 +1,11 @@
+//
+//  UITableViewCell+Support.swift
+//  VAYogaKitExample
+//
+//  Created by VAndrJ on 28.04.2024.
+//  Copyright © 2024 Volodymyr Andriienko. All rights reserved.
+//
+
+import UIKit
+
+extension UITableViewCell: VAIdentifiable {}
