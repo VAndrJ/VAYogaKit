3894394b3ab4f47053f9c29802cb81fa9f8e35e5
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-28 16:22:45 +0300

    [core] Indentation to insets mapping.

--- Sources/VAYogaKit/VAIndentation.swift
+++ Sources/VAYogaKit/VAIndentation.swift
@@ -36,3 +36,63 @@ public enum VAIndentation {
     /// Indentation defined using UIEdgeInsets.
     case insets(UIEdgeInsets)
 }
+
+public extension UIEdgeInsets {
+
+    /// Creates a `UIEdgeInsets` instance based on an array of indentation configurations.
+    ///
+    /// - Parameter indentation: An array of `VAIndentation` representing the padding values for each edge of the insets.
+    init(indentation: [VAIndentation]) {
+        var top: CGFloat = 0
+        var left: CGFloat = 0
+        var bottom: CGFloat = 0
+        var right: CGFloat = 0
+        indentation.forEach {
+            switch $0 {
+            case let .insets(insets):
+                top = insets.top
+                left = insets.left
+                bottom = insets.bottom
+                right = insets.right
+            case let .custom(paddingTop, paddingLeft, paddingBottom, paddingRight):
+                top = paddingTop
+                left = paddingLeft
+                bottom = paddingBottom
+                right = paddingRight
+            case let .vertical(padding):
+                top = padding
+                bottom = padding
+            case let .horizontal(padding):
+                left = padding
+                right = padding
+            case let .all(padding):
+                top = padding
+                left = padding
+                bottom = padding
+                right = padding
+            case let .bottom(padding):
+                bottom = padding
+            case let .top(padding):
+                top = padding
+            case let .left(padding):
+                left = padding
+            case let .right(padding):
+                right = padding
+            case let .topLeft(padding):
+                top = padding
+                left = padding
+            case let .topRight(padding):
+                top = padding
+                right = padding
+            case let .bottomLeft(padding):
+                left = padding
+                bottom = padding
+            case let .bottomRight(padding):
+                bottom = padding
+                right = padding
+            }
+        }
+
+        self.init(top: top, left: left, bottom: bottom, right: right)
+    }
+}
