df231a891ef09ef76e1b9a098e210be28d351690
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-29 10:43:48 +0300

    [example] Navigation identity.

--- VAYogaKitExample/VAYogaKitExample/Navigation/DefaultNavigationIdentity.swift
+++ VAYogaKitExample/VAYogaKitExample/Navigation/DefaultNavigationIdentity.swift
@@ -8,6 +8,8 @@
 
 import VANavigator
 
+typealias NavigationIdentity = VANavigator.NavigationIdentity
+
 protocol DefaultNavigationIdentity: NavigationIdentity {}
 
 extension DefaultNavigationIdentity {
