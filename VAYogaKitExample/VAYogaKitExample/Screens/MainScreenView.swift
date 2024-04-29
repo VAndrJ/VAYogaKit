b345ff68cdc19104145754712aa238b8dc0efac4
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-29 10:48:03 +0300

    [example] Main screen.

--- VAYogaKitExample/VAYogaKitExample/Screens/MainScreenView.swift
+++ VAYogaKitExample/VAYogaKitExample/Screens/MainScreenView.swift
@@ -6,4 +6,49 @@
 //  Copyright © 2024 Volodymyr Andriienko. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import VAYogaKit
+
+struct MainScreenNavigationIdentity: DefaultNavigationIdentity {}
+
+final class MainScreenView: BaseScreenView<MainScreenViewModel> {
+    private lazy var tableView = BaseTableView(
+        listData: viewModel.$listData,
+        onSelect: viewModel ?> { $0.onSelect(cell: $1) }
+    )
+
+    override var layout: any VAYogaLayout {
+        SafeArea {
+            tableView
+                .flex(grow: 1)
+        }
+    }
+
+    override func configure() {
+        backgroundColor = .systemBackground
+        controller?.title = "VAYogaKit examples"
+    }
+}
+
+final class MainScreenViewModel: BaseViewModel, UITableViewDelegate {
+    struct Context {
+        struct Navigation {
+            let navigate: (_ identity: NavigationIdentity) -> Void
+        }
+
+        let navigation: Navigation
+    }
+
+    @Published var listData: [MainTableCellViewModel] = [
+    ]
+
+    let context: Context
+
+    init(context: Context) {
+        self.context = context
+    }
+
+    func onSelect(cell: MainTableCellViewModel) {
+        context.navigation.navigate(cell.destination)
+    }
+}
