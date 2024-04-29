5e731875934d48e9bd0a398210274825c146bd75
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-29 11:14:21 +0300

    [core] Yoga scroll view.

--- Sources/VAYogaKit/VAYogaScrollView.swift
+++ Sources/VAYogaKit/VAYogaScrollView.swift
@@ -19,3 +19,37 @@ public struct VAYogaScrollableDirection: RawRepresentable, OptionSet, Sendable {
         self.rawValue = rawValue
     }
 }
+
+open class VAYogaScrollView: UIScrollView, VAYogaLayout {
+    public var layoutType: VAYogaLayoutType = .container
+    public var node: YGNodeRef!
+    public var sublayouts: [any VAYogaLayout] = []
+    public var layout: any VAYogaLayout { self }
+    public let contentView = UIView()
+    public var scrollableDirections: VAYogaScrollableDirection {
+        didSet { node.markDirtyIfAvailable() }
+    }
+
+    public init(scrollableDirections: VAYogaScrollableDirection = .vertical) {
+        self.scrollableDirections = scrollableDirections
+
+        super.init(frame: .init(x: 0, y: 0, width: 240, height: 128))
+
+        self.node = .new(for: self)
+        addSubview(contentView)
+    }
+    
+    @available(*, unavailable)
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+
+        flattenLayoutIfNeeded(in: contentView)
+        applyLayoutToScrollHierarchy(size: frame.size, scrollableDirections: scrollableDirections) {
+            contentView.frame.size = $0
+        }
+    }
+}
