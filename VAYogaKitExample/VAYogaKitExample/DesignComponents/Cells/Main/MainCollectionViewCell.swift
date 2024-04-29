586453055a94366d54cc054d104ff7e01871717e
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-29 19:40:06 +0300

    [example] Main collection view cell.

---
+++ VAYogaKitExample/VAYogaKitExample/DesignComponents/Cells/Main/MainCollectionViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  MainCollectionViewCell.swift
+//  VAYogaKitExample
+//
+//  Created by VAndrJ on 29.04.2024.
+//  Copyright © 2024 Volodymyr Andriienko. All rights reserved.
+//
+
+import Foundation
--- VAYogaKitExample/VAYogaKitExample/DesignComponents/Cells/Main/MainTableViewCell.swift
+++ VAYogaKitExample/VAYogaKitExample/DesignComponents/Cells/Main/MainTableViewCell.swift
@@ -35,7 +35,40 @@ class MainTableViewCell: VAYogaTableViewCell {
     }
 }
 
-class MainTableCellViewModel: SpecializedCellViewModel<MainTableViewCell, UICollectionViewCell> {
+class MainCollectionViewCell: VAYogaCollectionViewCell {
+    private let headerView = VAYogaView(layoutType: .view).apply {
+        $0.backgroundColor = .systemGray4
+        $0.flex(grow: 1)
+    }
+    private let titleLabel = VAYogaLabel().apply {
+        $0.numberOfLines = 0
+        $0.font = .boldSystemFont(ofSize: 18)
+    }
+    private let descriptionLabel = VAYogaLabel().apply {
+        $0.numberOfLines = 0
+        $0.font = .systemFont(ofSize: 15, weight: .light)
+        $0.textColor = .secondaryLabel
+    }
+
+    func configure(title: String, description: String) {
+        titleLabel.text = title
+        descriptionLabel.text = description
+    }
+
+    override var layout: any VAYogaLayout {
+        Column(spacing: 8, cross: .stretch) {
+            headerView
+            Column(spacing: 4, main: .end, cross: .stretch) {
+                titleLabel
+                descriptionLabel
+            }
+            .margin(.all(8))
+        }
+        .flex(grow: 1)
+    }
+}
+
+class MainTableCellViewModel: SpecializedCellViewModel<MainTableViewCell, MainCollectionViewCell> {
     let title: String
     let description: String
     let destination: NavigationIdentity
@@ -47,6 +80,10 @@ class MainTableCellViewModel: SpecializedCellViewModel<MainTableViewCell, UIColl
     }
 
     override func configure(cell: MainTableViewCell) {
+        cell.configure(title: title, description: description)
+    }
+
+    override func configure(cell: MainCollectionViewCell) {
         cell.configure(title: title, description: description)
     }
 }
