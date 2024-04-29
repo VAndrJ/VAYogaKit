efa7e7af2f120db975c8cbf009631df781818223
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-29 10:44:25 +0300

    [example] Main table view cell.

--- VAYogaKitExample/VAYogaKitExample/DesignComponents/Cells/MainTableViewCell.swift
+++ VAYogaKitExample/VAYogaKitExample/DesignComponents/Cells/MainTableViewCell.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2024 Volodymyr Andriienko. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import VAYogaKit
+
+class MainTableViewCell: VAYogaTableViewCell {
+    private let titleLabel = VAYogaLabel().apply {
+        $0.numberOfLines = 0
+        $0.font = .boldSystemFont(ofSize: 20)
+    }
+    private let descriptionLabel = VAYogaLabel().apply {
+        $0.numberOfLines = 0
+        $0.font = .systemFont(ofSize: 16, weight: .light)
+    }
+
+    func configure(title: String, description: String) {
+        accessoryType = .disclosureIndicator
+        titleLabel.text = title
+        descriptionLabel.text = description
+    }
+
+    override var layout: any VAYogaLayout {
+        Column(spacing: 8) {
+            titleLabel
+            descriptionLabel
+        }
+        .margin(.all(16))
+    }
+}
