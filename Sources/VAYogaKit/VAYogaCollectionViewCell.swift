5774a89bc842c92487f204202527cde82a06e1f5
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-29 16:13:28 +0300

    [core] Yoga collection view cell.

--- Sources/VAYogaKit/VAYogaCollectionViewCell.swift
+++ Sources/VAYogaKit/VAYogaCollectionViewCell.swift
@@ -5,4 +5,48 @@
 //  Created by VAndrJ on 29.04.2024.
 //
 
-import Foundation
+import UIKit
+import yoga
+
+open class VAYogaCollectionViewCell: UICollectionViewCell, VAYogaLayout {
+    public var layoutType: VAYogaLayoutType = .container
+    public var node: YGNodeRef!
+    public var sublayouts: [any VAYogaLayout] = []
+    open var layout: any VAYogaLayout { self }
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        self.node = .new(for: self)
+        flattenLayoutIfNeeded(in: contentView)
+    }
+
+    @available(*, unavailable)
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public override func layoutSubviews() {
+        flattenLayoutIfNeeded(in: contentView)
+
+        super.layoutSubviews()
+
+        // TODO: - For different sizings
+        applyLayoutToCollectionCellHierarchy(size: contentView.frame.size) { size in
+            frame.size.height = size.height
+        }
+    }
+
+    public override func sizeThatFits(_ size: CGSize) -> CGSize {
+        flattenLayoutIfNeeded(in: contentView)
+        applyLayoutToCollectionCellHierarchy(size: contentView.frame.size) { size in
+            frame.size.height = size.height
+        }
+
+        return frame.size
+    }
+
+    deinit {
+        YGNodeFree(node)
+    }
+}
