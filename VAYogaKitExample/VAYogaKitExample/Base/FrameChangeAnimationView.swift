4602d253e09b9ca0f345fbd0dd6e2b27899d4935
Volodymyr Andriienko <vandrjios@gmail.com>
2024-05-01 22:59:57 +0300

    [example] Frame change animation view.

--- VAYogaKitExample/VAYogaKitExample/Base/FrameChangeAnimationView.swift
+++ VAYogaKitExample/VAYogaKitExample/Base/FrameChangeAnimationView.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2024 Volodymyr Andriienko. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class FrameChangeAnimationView: BaseView {
+    var animationDuration = 0.3
+    var isInitialSkipped = false
+
+    override var frame: CGRect {
+        get { super.frame }
+        set {
+            if isInitialSkipped {
+                UIView.animate(
+                    withDuration: animationDuration,
+                    delay: 0,
+                    options: [.beginFromCurrentState, .curveLinear]
+                ) {
+                    super.frame = newValue
+                }
+            } else {
+                if frame.size != newValue.size {
+                    super.frame = newValue
+                    isInitialSkipped = true
+                }
+            }
+        }
+    }
+}
